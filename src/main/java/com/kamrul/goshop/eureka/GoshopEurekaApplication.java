0ade2d106bbfeab65e2fbf0499a2cbaf30c6be12
Kamrul Bari <69103450+kamrulbari14@users.noreply.github.com>
2025-01-20 00:38:15 +0600

    chore: initiate project

---
+++ src/main/java/com/kamrul/goshop/eureka/GoshopEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.kamrul.goshop.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoshopEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoshopEurekaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kamrul/goshop/eureka/GoshopEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamrul.goshop.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoshopEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
